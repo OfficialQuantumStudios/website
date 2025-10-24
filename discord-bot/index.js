f864956ebcdd3ec70dee8020ea2bb2eb4032b709
mysterek_ <59736397+mysterek1337@users.noreply.github.com>
2025-10-24 21:30:29 +0200

    Update index.js

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = 4040;
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
